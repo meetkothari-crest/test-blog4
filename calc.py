9141877c4498a7104170e410fc44255fb35f3bc5
meetkothari-crest <meet.k@crestskillserve.com>
2025-03-11 10:56:18 +0530

    Remediated code

--- calc.py
+++ calc.py
@@ -3,8 +3,8 @@ def add(a, b):
     return c
 
 def subtract(a, b):
-    c =  a - b
-    return c
+    c = a - b
+    return c  
 
 def multiply(a, b):
     return a * b
@@ -12,15 +12,13 @@ def multiply(a, b):
 def divide(a, b):
     if b == 0:
         return "Error! Division by zero."
-    c =  a / b
-    return c
+    return a / b
 
-# Example usage
-num1 = input("Enter first number: ")
+num1 = float(input("Enter first number: "))  
 num2 = float(input("Enter second number: "))
 
 
 print(add(num1, num2))
-print(subtract(num1, num2))
+print(subtract(num1, num2)) 
 print(multiply(num1, num2))
 print(divide(num1, num2))
